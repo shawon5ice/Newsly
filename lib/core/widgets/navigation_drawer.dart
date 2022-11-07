42100c4665113dad1ffd46621d0f1485e2f8b69f
Arman Ahmed Shawon <shawon5ice@gmail.com>
2022-11-08 01:02:29 +0600

    animated theme changing.

--- lib/core/widgets/navigation_drawer.dart
+++ lib/core/widgets/navigation_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -79,8 +81,6 @@ import '../session/session_manager.dart';
 //   }
 // }
 
-
-
 class NavDraw extends StatefulWidget {
   @override
   _NavDrawState createState() => _NavDrawState();
@@ -88,8 +88,24 @@ class NavDraw extends StatefulWidget {
 
 int selectedIndex = 0;
 
-class _NavDrawState extends State<NavDraw> {
+class _NavDrawState extends State<NavDraw> with TickerProviderStateMixin {
   var session = locator<SessionManager>();
+
+  @override
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(microseconds: 1000),
+    vsync: this,
+  )..reverse(from: 45);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,22 +114,59 @@ class _NavDrawState extends State<NavDraw> {
       width: .6.sw,
       child: ListView(
         children: <Widget>[
-          Container(
-            color: Colors.black87,
-            height: 200.0,
-            child: DrawerHeader(
-                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                padding: EdgeInsets.zero,
-                 child: SvgPicture.asset('assets/svg/newsly.svg'),),
+          Stack(
+            children: [
+              Container(
+                color: Colors.black45,
+                height: 200.0,
+                child: DrawerHeader(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  padding: EdgeInsets.zero,
+                  child: SvgPicture.asset(
+                    'assets/svg/newsly.svg',
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 16,
+                right: 16,
+                child: RotationTransition(
+                  
+                  turns: _animation,
+                  child: Transform.rotate(
+                    angle: -0.5,
+                    child: ThemeSwitcher(
+                      builder: (context) => GestureDetector(
+                        onTap: () {
+                          var brightness = ThemeModelInheritedNotifier.of(context)
+                              .theme
+                              .brightness;
+                          ThemeSwitcher.of(context).changeTheme(
+                              theme: brightness == Brightness.light
+                                  ? NewslyThemeData.dark()
+                                  : NewslyThemeData.light());
+                          switchValue = !switchValue;
+                          session.darkTheme = switchValue;
+                        },
+                        child: Icon(
+                          !switchValue ? Icons.nightlight_round : Icons.sunny,
+                          color: !switchValue ? Colors.white : Colors.orange,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
           _createDrawerItem(
               icon: Icons.home_outlined,
               text: 'Home',
               isSelected: selectedIndex == 0,
               onTap: () {
-                if(selectedIndex != 0){
+                if (selectedIndex != 0) {
                   Navigator.pushNamed(context, home);
-                }else{
+                } else {
                   Navigator.pop(context);
                 }
                 setState(() {
@@ -125,9 +178,9 @@ class _NavDrawState extends State<NavDraw> {
               text: 'Bookmarks',
               isSelected: selectedIndex == 1,
               onTap: () {
-                if(selectedIndex != 1){
+                if (selectedIndex != 1) {
                   Navigator.pushNamed(context, bookmarkPage);
-                }else{
+                } else {
                   Navigator.pop(context);
                 }
                 setState(() {
@@ -136,25 +189,23 @@ class _NavDrawState extends State<NavDraw> {
               }),
           ListTile(
             leading: Icon(Icons.nightlight_round),
-            title: Text(switchValue?'Dark':'Light'),
+            title: Text(switchValue ? 'Dark' : 'Light'),
             trailing: ThemeSwitcher(
-                builder: (context)=> Switch.adaptive(
-                  value: switchValue,
-                  onChanged: (value){
-                    var brightness =
-                        ThemeModelInheritedNotifier.of(context)
+                builder: (context) => Switch.adaptive(
+                      value: switchValue,
+                      onChanged: (value) {
+                        var brightness = ThemeModelInheritedNotifier.of(context)
                             .theme
                             .brightness;
-                    ThemeSwitcher.of(context).changeTheme(
-                        theme: brightness == Brightness.light?
-                        NewslyThemeData.dark():NewslyThemeData.light());
-                    switchValue = !switchValue;
-                    session.darkTheme = switchValue;
-                  },
-                )
-            ),
+                        ThemeSwitcher.of(context).changeTheme(
+                            theme: brightness == Brightness.light
+                                ? NewslyThemeData.dark()
+                                : NewslyThemeData.light());
+                        switchValue = !switchValue;
+                        session.darkTheme = switchValue;
+                      },
+                    )),
           ),
-
         ],
       ),
     );
@@ -162,19 +213,23 @@ class _NavDrawState extends State<NavDraw> {
 }
 
 Widget _createDrawerItem(
-    {required IconData icon, required String text, required GestureTapCallback onTap, required bool isSelected}) {
+    {required IconData icon,
+    required String text,
+    required GestureTapCallback onTap,
+    required bool isSelected}) {
   return ListTile(
-      selectedTileColor: isSelected ? NewslyThemeData.primaryColor : Colors.transparent,
-      selected: true,
-      title: Row(
-        children: <Widget>[
-          Icon(icon),
-          Padding(
-            padding: EdgeInsets.only(left: 8.0),
-            child: Text(text),
-          )
-        ],
-      ),
-      onTap: onTap,
+    selectedTileColor:
+        isSelected ? NewslyThemeData.cardColor : Colors.transparent,
+    selected: true,
+    title: Row(
+      children: <Widget>[
+        Icon(icon),
+        Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Text(text),
+        )
+      ],
+    ),
+    onTap: onTap,
   );
 }
--- lib/details/presentation/ui/news_details.dart
+++ lib/details/presentation/ui/news_details.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:newsly/core/logger/logger.dart';
 import 'package:newsly/core/theme/newsly_theme_data.dart';
 import 'package:newsly/core/utils/constants.dart';
 import 'package:newsly/details/data/model/news_details_model.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../../core/service/bookmark/bookmark.dart';
 import '../../../core/service/hive_boxes.dart';
@@ -69,136 +71,163 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('By ${news.author}'),
+      appBar: AppBar(
+        title: Text(
+          'By ${news.author}',
+          style: GoogleFonts.kaushanScript(
+            textStyle:
+                const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
         ),
-        body: SafeArea(
-            child: SingleChildScrollView(
-                child: Container(
-                    child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                news.title.toString(),
-                textAlign: TextAlign.justify,
-                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24),
-              ),
-            ),
-          ),
-          Row(
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              SizedBox(width: 10),
-              FaIcon(
-                FontAwesomeIcons.clock,
-                color: NewslyThemeData.primaryColor,
-              ),
-              SizedBox(width: 10),
-              Text(DateFormat.yMMMd()
-                      .format(DateTime.parse(news.publishedAt.toString())) +
-                  " At " +
-                  DateFormat('hh:mm a')
-                      .format(DateTime.parse(news.publishedAt.toString())))
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Stack(
-            children: [
-              Hero(
-                tag: news.urlToImage.toString(),
-                child: Image.network(
-                  loadingBuilder: (BuildContext context, Widget child,
-                      ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Container(
-                      height: 300,
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                                  loadingProgress.expectedTotalBytes!
-                              : null,
-                        ),
-                      ),
-                    );
-                  },
-                  news.urlToImage.toString(),
-                  height: 300,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 10,
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if (isBookmarked) {
-                          isBookmarked = false;
-                          var _task = box.values
-                              .where((element) =>
-                                  news.publishedAt == element.publishedAt)
-                              .first;
-                          _task.delete();
-                        } else {
-                          addBookmark(news);
-                          logger.printDebugLog("Bookmarked...");
-                          isBookmarked = true;
-                        }
-                        setState(() {});
-                      },
-                      child: Card(
-                        color: Colors.white,
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(
-                            isBookmarked
-                                ? Icons.bookmark
-                                : Icons.bookmark_outline,
-                            size: 40,
-                            color: isBookmarked ? Colors.green : Colors.black,
-                          ),
-                        ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Column(mainAxisSize: MainAxisSize.min, children: [
+                  Flexible(
+                    child: Text(
+                      news.title.toString(),
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Share.share('check out this news ${news.url}', subject: 'Look what this news saying...');
-                      },
-                      child: Card(
-                        color: Colors.white,
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(
-                            Icons.ios_share_outlined,
-                            size: 40,
-                            color: NewslyThemeData.primaryColor,
-                          ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: 10),
+                      FaIcon(
+                        FontAwesomeIcons.clock,
+                        color: NewslyThemeData.primaryColor,
+                      ),
+                      SizedBox(width: 10),
+                      Text(DateFormat.yMMMd()
+                              .format(DateTime.parse(news.publishedAt.toString())) +
+                          " At " +
+                          DateFormat('hh:mm a')
+                              .format(DateTime.parse(news.publishedAt.toString())))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Stack(
+                    children: [
+                      Hero(
+                        tag: news.urlToImage.toString(),
+                        child: Image.network(
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return Container(
+                              height: 300,
+                              child: Center(
+                                child: CircularProgressIndicator(
+                                  value: loadingProgress.expectedTotalBytes != null
+                                      ? loadingProgress.cumulativeBytesLoaded /
+                                          loadingProgress.expectedTotalBytes!
+                                      : null,
+                                ),
+                              ),
+                            );
+                          },
+                          news.urlToImage.toString(),
+                          height: 300,
+                          fit: BoxFit.cover,
                         ),
                       ),
+                      Positioned(
+                        bottom: 0,
+                        right: 10,
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                if (isBookmarked) {
+                                  isBookmarked = false;
+                                  var _task = box.values
+                                      .where((element) =>
+                                          news.publishedAt == element.publishedAt)
+                                      .first;
+                                  _task.delete();
+                                } else {
+                                  addBookmark(news);
+                                  logger.printDebugLog("Bookmarked...");
+                                  isBookmarked = true;
+                                }
+                                setState(() {});
+                              },
+                              child: Card(
+                                color: Colors.white,
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Icon(
+                                    isBookmarked
+                                        ? Icons.bookmark
+                                        : Icons.bookmark_outline,
+                                    size: 40,
+                                    color:
+                                        isBookmarked ? Colors.green : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Share.share('check out this news ${news.url}',
+                                    subject: 'Look what this news saying...');
+                              },
+                              child: Card(
+                                color: Colors.white,
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Icon(
+                                    Icons.ios_share_outlined,
+                                    size: 40,
+                                    color: NewslyThemeData.primaryColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      news.description.toString(),
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal),
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                ]),
+              ),
+              WebView(
+                initialUrl: news.url,
               )
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              news.description.toString(),
-              style: TextStyle(fontSize: 18),
-            ),
-          )
-        ])))));
+        ),
+      ),
+    );
   }
 
   addBookmark(NewsDetails news) {
@@ -227,11 +256,10 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
         title: 'Example share',
         text: 'Example share text',
         linkUrl: 'https://flutter.dev/',
-        chooserTitle: 'Example Chooser Title'
-    );
+        chooserTitle: 'Example Chooser Title');
   }
 
-  _onShare(String text) async{
+  _onShare(String text) async {
     final box = context.findRenderObject() as RenderBox?;
 
     await Share.share(
