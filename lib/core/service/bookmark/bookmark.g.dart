0d73b0b5e09c6123b3e62940d7784c6fa824ccca
Arman Ahmed Shawon <shawon5ice@gmail.com>
2022-11-10 01:38:11 +0600

    Screenshot added

---
+++ lib/core/service/bookmark/bookmark.g.dart
@@ -0,0 +1,61 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bookmark.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class BookmarkAdapter extends TypeAdapter<Bookmark> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Bookmark read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Bookmark()
+      ..source = fields[0] as String
+      ..author = fields[1] as String
+      ..title = fields[2] as String
+      ..description = fields[3] as String
+      ..url = fields[4] as String
+      ..urlToImage = fields[5] as String
+      ..publishedAt = fields[6] as String
+      ..content = fields[7] as String;
+  }
+
+  @override
+  void write(BinaryWriter writer, Bookmark obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.source)
+      ..writeByte(1)
+      ..write(obj.author)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.url)
+      ..writeByte(5)
+      ..write(obj.urlToImage)
+      ..writeByte(6)
+      ..write(obj.publishedAt)
+      ..writeByte(7)
+      ..write(obj.content);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BookmarkAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
